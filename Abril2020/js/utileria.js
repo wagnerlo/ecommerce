2dcd73bc3485e740d2318308c4a85a029a8db81a
josecaos <jazzvoon@gmail.com>
2020-04-18 12:31:50 -0500

    Funcion agregarResultado

--- Abril2020/js/utileria.js
+++ Abril2020/js/utileria.js
@@ -1,19 +1,23 @@
-jQuery(document).ready(function(){
+// document.addEventListener("DOMContentLoaded", () => {
+
+// })  
+jQuery(document).ready(() => {
+
     // This button will increment the value
-    $('.qtyplus').click(function(e){
+    $('.qtyplus').click(function(e) {
         // Stop acting like a button
         e.preventDefault();
         // Get the field name
         fieldName = $(this).attr('field');
         // Get its current value
-        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
         // If is not undefined
         if (!isNaN(currentVal)) {
             // Increment
-            $('input[name='+fieldName+']').val(currentVal + 1);
+            $('input[name=' + fieldName + ']').val(currentVal + 1);
         } else {
             // Otherwise put a 0 there
-            $('input[name='+fieldName+']').val(0);
+            $('input[name=' + fieldName + ']').val(0);
         }
     });
     // This button will decrement the value till 0
@@ -23,15 +27,48 @@ jQuery(document).ready(function(){
         // Get the field name
         fieldName = $(this).attr('field');
         // Get its current value
-        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
         // If it isn't undefined or its greater than 0
         if (!isNaN(currentVal) && currentVal > 0) {
             // Decrement one
-            $('input[name='+fieldName+']').val(currentVal - 1);
+            $('input[name=' + fieldName + ']').val(currentVal - 1);
         } else {
             // Otherwise put a 0 there
-            $('input[name='+fieldName+']').val(0);
+            $('input[name=' + fieldName + ']').val(0);
         }
     });
-});
 
+
+    agregarCantidad();
+
+}); // ready
+
+// 
+const agregarCantidad = () => {
+
+    let cantidad;
+    let botones = document.querySelectorAll('.botonAgrega'); //regresa un array
+    let respuesta = document.querySelector('#cantidad');
+
+
+    botones.forEach((boton) => {
+
+        if (boton.classList.contains('qtyminus')) {
+
+            boton.addEventListener("click", (elemento) => {
+
+                cantidad = elemento.target.parentNode.nextElementSibling.childNodes[1].value;
+                respuesta.innerHTML = cantidad
+            })
+        } else if (boton.classList.contains('qtyplus')) {
+            boton.addEventListener("click", (elemento) => {
+
+                cantidad = elemento.target.parentNode.previousElementSibling.childNodes[1].value;
+                respuesta.innerHTML = cantidad
+            })
+        }
+
+
+    })
+
+};
